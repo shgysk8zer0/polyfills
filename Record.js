4bb19ee3339a05cd8500eebfed2257b27e4e0594
Chris Zuber <shgysk8zer0@gmail.com>
2023-12-10 17:10:00 -0800

    Fix misc issues w Records & Tuples

--- Record.js
+++ Record.js
@@ -1,6 +1,10 @@
 if (! (globalThis.Record instanceof Function)) {
 	/* eslint-disable no-inner-declarations */
 	function Record(obj) {
+		if (new.target === Record) {
+			throw new TypeError('Record is not a constructor');
+		}
+
 		const record = Object.create(Record.prototype);
 		Object.defineProperties(record, Object.fromEntries(Object.entries(obj).map(([key, value]) => [
 			key, {
@@ -15,10 +19,16 @@ if (! (globalThis.Record instanceof Function)) {
 		return record;
 	}
 
-	Record.prototype = { constructor: Record };
+	Record.prototype.constructor  = Record;
+	Object.defineProperty(Record.prototype, Symbol.toStringTag, {
+		value: 'Record',
+		enumerable: true,
+		configurable: false,
+		writable: false,
+	});
 
 	Record.fromEntries = function fromEntries(entries) {
-		return new Record(Object.fromEntries(entries));
+		return Record(Object.fromEntries(entries));
 	};
 
 	globalThis.Record = Record;
